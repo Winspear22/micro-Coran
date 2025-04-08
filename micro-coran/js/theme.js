d878fca3b69787e0708ad05d2be4a727bcf83f8f
Winspear22 <adnen.aloui2@hotmail.com>
2025-04-08 15:20:37 +0200

    début du site Coran avec la sourate Al Fatiha

---
+++ micro-coran/js/theme.js
@@ -0,0 +1,20 @@
+function repeatVerse(id) {
+    const audio = document.getElementById(id);
+    audio.loop = true;
+    audio.play();
+  }
+  
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("theme-toggle");
+    const body = document.body;
+  
+    if (localStorage.getItem("theme") === "dark") {
+      body.classList.add("dark");
+    }
+  
+    toggle.addEventListener("click", () => {
+      body.classList.toggle("dark");
+      localStorage.setItem("theme", body.classList.contains("dark") ? "dark" : "light");
+    });
+  });
+  
